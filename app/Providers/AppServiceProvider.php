635d803a0d3b87f8d354ea064def0f2a6c06172c
makoo7 <makoo7.prajapati@gmail.com>
2024-01-01 21:02:01 +0530

    changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-require_once("../vendor/phpoffice/phppresentation/src/PhpPresentation/Autoloader.php");
-require_once("../vendor/phpoffice/common/src/Common/Autoloader.php");
+// require_once("../vendor/phpoffice/phppresentation/src/PhpPresentation/Autoloader.php");
+// require_once("../vendor/phpoffice/common/src/Common/Autoloader.php");
 
 class AppServiceProvider extends ServiceProvider
 {
