462b925fbd6506c289d053c82be30a749c1abae9
mitesh prajapati <makoo7.prajapati@gmail.com>
2024-02-01 16:37:47 +0530

    Update SampleReportLink.php
    github test changes

--- app/Mail/SampleReportLink.php
+++ app/Mail/SampleReportLink.php
@@ -7,18 +7,25 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class SampleReportLink extends Mailable
+class sampleReportLink extends Mailable
 {
     use Queueable, SerializesModels;
+    public $report;
+    public $sampleId;
+    public $user;
+    public $slug;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($report, $sampleId, $user, $slug)
     {
-        //
+        $this->report = $report;
+        $this->sampleId = $sampleId;
+        $this->user = $user;
+        $this->slug = $slug;
     }
 
     /**
@@ -28,6 +35,8 @@ class SampleReportLink extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.front.sample_report_link', ['report' => $this->report, 'sampleId' => $this->sampleId,
+        'user' => $this->user, 'slug' => $this->slug])
+        ->subject('Sample Report Link');
     }
 }
