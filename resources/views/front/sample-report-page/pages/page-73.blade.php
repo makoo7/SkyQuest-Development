6c918ceeb23a8d57873b8ed69285cec613f173a6
mitesh prajapati <makoo7.prajapati@gmail.com>
2024-02-19 23:54:44 +0530

    slide 73 added as img

--- resources/views/front/sample-report-page/pages/page-73.blade.php
+++ resources/views/front/sample-report-page/pages/page-73.blade.php
@@ -1,10 +1,5 @@
 <div id="pg2Overlay" style="width:100%; height:100%; z-index:1; background-color:rgba(0,0,0,0); -webkit-user-select: none;"></div>
 <div id="pg2" class="content-background" style="-webkit-user-select: none;"></div>
-<div class="text-container">
-    <div class="d-flex justify-content-between align-items-center mb-5">
-        <p class="intro-frame page-title page-5-title-full">{{ $response["data"]["title"] }}</p>
-        <div>
-            <img src="{{ asset("assets/frontend/slide/2/img/1.jpg") }}" alt="logo" class="img-fluid" />
-        </div>
-    </div>
+<div class="text-container p-0">
+    <img src="{{ asset("assets/frontend/slide/73/1.png") }}" alt="logo" class="img-fluid w-100 h-100" />
 </div>
